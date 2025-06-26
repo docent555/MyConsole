f1a7dbc9212cc93e5bb5b96ae5a43599179dd6e6
DESKTOP-5E7409A\Alex <docent748@gmail.com>
2025-06-26 14:23:56 +0300

    Новая ветка

--- MyConsole/Program.cs
+++ MyConsole/Program.cs
@@ -16,6 +16,8 @@
 
             WriteLine();
 
+            Console.WriteLine("Новая ветка.");
+
             priceUp = decimal.Parse(ReadLine("Задайте верхнюю цену: "));
 
             priceDown = int.Parse(ReadLine("Введите нижнюю цену: "));
