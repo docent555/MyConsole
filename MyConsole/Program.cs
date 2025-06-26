7e98a4014dd200d3496f083971929b4fdb915a80
DESKTOP-5E7409A\Alex <docent748@gmail.com>
2025-06-26 14:11:11 +0300

    Добавьте файлы проекта.

---
+++ MyConsole/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace MyConsole
+{
+    internal class Program
+    {
+        static decimal priceUp;
+
+        static decimal priceDown;
+
+        static decimal stepLevel;
+
+        static List<decimal> levels;
+
+        static void Main(string[] args)
+        {
+            levels = new List<decimal>();
+
+            WriteLine();
+
+            priceUp = decimal.Parse(ReadLine("Задайте верхнюю цену: "));
+
+            priceDown = int.Parse(ReadLine("Введите нижнюю цену: "));
+
+            StepLevel = decimal.Parse(ReadLine("Введите шаг уровня: "));
+
+            WriteLine();
+        }
+
+        static decimal StepLevel
+        {
+            get
+            {
+                return stepLevel;
+            }
+
+            set
+            {
+                stepLevel = value;
+
+                decimal priceLevel = priceUp;
+
+                while (priceLevel >= priceDown)
+                {
+                    levels.Add(priceLevel);
+
+                    priceLevel -= stepLevel;
+                }
+            }
+        }
+
+        static void WriteLine()
+        {
+            Console.WriteLine("\nКол-во элементов в списке: " + levels.Count.ToString() + "\n");
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                Console.WriteLine(levels[i]);
+            }
+        }
+
+        static string ReadLine(string message)
+        {
+            Console.Write(message);
+
+            string str = Console.ReadLine() ?? "0";
+
+            return str;
+        }
+    }
+}
+
